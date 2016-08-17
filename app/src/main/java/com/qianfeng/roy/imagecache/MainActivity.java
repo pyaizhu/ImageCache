485e123cc7033b46e8987a25a983004cd82f6e33
Administrator <854982469@qq.com>
2016-08-17 16:40:10 +0800

    提交diyici

--- app/src/main/java/com/qianfeng/roy/imagecache/MainActivity.java
+++ app/src/main/java/com/qianfeng/roy/imagecache/MainActivity.java
@@ -3,7 +3,6 @@ package com.qianfeng.roy.imagecache;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,10 +12,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        for (int i = 0; i <10 ; i++) {
-            Toast.makeText(MainActivity.this, "123"+i, Toast.LENGTH_SHORT).show();
-        }
-        //你好
         setContentView(R.layout.activity_main);
         ListView lv = (ListView) findViewById(R.id.lv);
         List<String> data = new ArrayList<>();
