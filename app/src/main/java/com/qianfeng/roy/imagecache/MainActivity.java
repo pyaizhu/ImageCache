8861d2a9d50106de8b5ea700443f2fe6bfc8533b
Administrator <854982469@qq.com>
2016-08-17 16:21:00 +0800

    disi

--- app/src/main/java/com/qianfeng/roy/imagecache/MainActivity.java
+++ app/src/main/java/com/qianfeng/roy/imagecache/MainActivity.java
@@ -3,6 +3,7 @@ package com.qianfeng.roy.imagecache;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+            Toast.makeText(MainActivity.this, "123", Toast.LENGTH_SHORT).show();
 
         //你好
         setContentView(R.layout.activity_main);
